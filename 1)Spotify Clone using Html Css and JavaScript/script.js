30ef57ca75eee079898baac443cef66c8945b796
Faysal <ezazfaysal03@gmail.com>
2023-10-10 01:26:42 +0530

    Updating the js file
    But the music is not playing, some issue is still there.
    Trying to fix it.
    Next Commit will be after i fix the issue.

--- 1)Spotify Clone using Html Css and JavaScript/script.js
+++ 1)Spotify Clone using Html Css and JavaScript/script.js
@@ -1,1 +1,33 @@
 console.log("Welcome to Spotify"); 
+
+// Initializing the variables
+let songIndex = 0; /* kaunsa song play ho rha hai*/
+let audioElement = new Audio('1.mp3'); /* */
+let masterPlay = document.getElementById('masterPlay'); /* */
+let myProgressBar = document.getElementById('myProgressBar')
+
+let songs = [
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath},
+    {songName: "Salam-e-Ishq", filePath: "1.mp3", coverPath}
+]
+// audioElement.play() 
+
+// Handle Play/Pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play(); 
+    }
+})
+// Listening to Events
+myProgressBar.addEventListener('timeUpdate', ()=>{
+    console.log('timeUpdate');
+    // Update Seekbar
+})
