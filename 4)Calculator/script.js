d30ed83f6c5daf15e82c68aa951bd8f7a70dc43a
Faysal <ezazfaysal03@gmail.com>
2023-12-18 04:27:29 +0530

    working on the js file

---
+++ 4)Calculator/script.js
@@ -0,0 +1,17 @@
+let string = ""; // Empty String.
+let buttons = document.querySelectorAll('.button'); 
+// query selector all un sare buttons ko select kar lenegi jinki class '.button' hain.
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target);
+            document.querySelector('input').value = string;
+            string = string + e.target.innerHTML;
+        }
+    })
+
+})
