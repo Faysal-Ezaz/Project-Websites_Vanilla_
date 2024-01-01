58e51b56c42c1f4a4850d857de93c5a80c76d666
Faysal <ezazfaysal03@gmail.com>
2024-01-02 03:35:35 +0530

    The navbar design is done. Added the hamburger style sidebar too.

---
+++ 6)SimpleNavbar/New_NavBar_1/main.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSideBar(){
+    const Sidebar = document.querySelector('.sidebar'); 
+    Sidebar.style.display = 'none';
+}
