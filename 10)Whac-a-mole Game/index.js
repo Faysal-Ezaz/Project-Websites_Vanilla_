76056dbead48c67047d1ca55246d09ccd579fff7
Faysal <ezazfaysal03@gmail.com>
2024-11-11 17:32:05 +0530

    There is an issue in the code. Cant seem to get a hold of this code.

--- 10)Whac-a-mole Game/index.js
+++ 10)Whac-a-mole Game/index.js
@@ -1,3 +1,5 @@
+let currMoleTile; // This helps keep track of which tile has the mole.
+
 
 
 
@@ -13,4 +15,21 @@ function setGame(){
         tile.id = i.toString(); // assigning id to the div tag from '0-8' 
         document.getElementById('board').appendChild(tile);
     }
+    setInterval(setMole, 2000); // after every 2 seconds.
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString(); // returned in string format so it can be used with `tile id`
+}
+
+function setMole() {
+
+    let mole = document.createElement("img");
+    mole.src =  "./monty-mole.png"; 
+
+    let num = getRandomTile(); 
+    currMoleTile = document.getElementById(num); 
+    currMoleTile.appendChild(mole); 
+
 }
